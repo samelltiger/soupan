86b90ead17668004dc1462acd5d0e08ab40e7bef
samelltiger <1176079760@qq.com>
2017-09-08 23:43:18 +0800

    fixed a error

--- pyspider/lib/proxyFun.py
+++ pyspider/lib/proxyFun.py
@@ -98,7 +98,7 @@ def testProxy(conn):
 			
 			print(get_one_proxy)
 			
-		except (urllib.error.URLError,http.client.RemoteDisconnected):
+		except (urllib.error.URLError,http.client.RemoteDisconnected,ConnectionResetError):
 			conn.remove({"_id":get_one_proxy["_id"]})
 			print("remove:",get_one_proxy)
 			# print(err.args)
