ff5c7bf9c786675b77d7641c3d0d401ea98aa706
samelltiger <1176079760@qq.com>
2017-09-20 21:17:09 +0800

    add kill exe

---
+++ kill_exe.sh
@@ -0,0 +1,14 @@
+processname="soupan"
+
+#all_time = $[`ps aux|grep "$processname"|grep -v grep|awk '{print $9}'`]
+#echo $all_time
+
+#for tim in $all_time;do
+#echo $tim
+#done
+
+for pid in $(ps aux |grep $processname |grep -v grep|awk '{print $2}'); do
+kill -9 $pid
+done
+
+/etc/init.d/mongod restart
